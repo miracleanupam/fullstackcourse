067d9bc451dbca8c2d21ae723ab140266f0c3e5d
anupam <miracleanupam@gmail.com>
2020-09-16 15:29:59 +0545

    exercise 2.1, componenet for Course, and hierarchy of components

---
+++ part02/course_info/src/index.js
@@ -0,0 +1,74 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <ul>
+        {parts.map((part) => (
+          <li key={part.id}>
+            <Part name={part.name} exercises={part.exercises} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises:{" "}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
