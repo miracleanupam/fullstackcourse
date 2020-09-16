3765bb35de58ae015253057eccbf070ff48a1fb1
anupam <miracleanupam@gmail.com>
2020-09-16 15:52:22 +0545

    exercise 2.3, use of reduce() for displaying total exercises

--- part02/course_info/src/index.js
+++ part02/course_info/src/index.js
@@ -27,10 +27,22 @@ const Content = ({ parts }) => {
   );
 };
 
+
+// array.reduce takes first two elements of array and applies
+// the function, result is kept in 'sum' when initial value for
+// sum is not provided. I have provided the initial value as 0 
+// Now reduce will take each element of the array one at a time
+// and apply the function and update 'sum' with result
 const Total = ({ parts }) => {
   return (
     <h4>
-      Total of {parts[0].exercises + parts[1].exercises + parts[2].exercises + parts[3].exercises} exercises
+      Total of {" "}
+      {parts.reduce(
+        (sum, part) => {
+          return sum + part.exercises;
+        }, 0
+      )} {" "}
+      exercises
     </h4>
   );
 };
