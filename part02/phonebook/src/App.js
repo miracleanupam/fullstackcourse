5e675a1d8f888a21f27a2054ed2a36e03fa2be9a
anupam <miracleanupam@gmail.com>
2020-09-17 19:46:00 +0545

    exercise 2.9 and 2.10, filter feature added, and each component is separated from <App /> component. Also there are minor variable names refactoring

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
 
+const Filter = ({ search, handleSearchInput, handleSearch }) => {
+  return (
+    <div>
+      <form onSubmit={handleSearch}>
+        Filter shown with <input value={search} onChange={handleSearchInput} />
+      </form>
+    </div>
+  );
+};
+
 const AddPersonForm = ({
   addPerson,
   handleNameInput,
@@ -9,6 +19,7 @@ const AddPersonForm = ({
 }) => {
   return (
     <form onSubmit={addPerson}>
+      <h4>Add a new</h4>
       <div>
         Name: <input value={newName} onChange={handleNameInput} />
       </div>
@@ -22,7 +33,7 @@ const AddPersonForm = ({
   );
 };
 
-const AllPersons = ({ persons }) => {
+const PersonDetails = ({ persons }) => {
   return (
     <div>
       <h2>Numbers</h2>
@@ -38,26 +49,42 @@ const AllPersons = ({ persons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 0, name: "Arto Hellas", number: "040-1234567" },
+  // this state is used to store the current/filtered details of phonebook
+  const [filteredPersons, setFilteredPersons] = useState([
+    { id: 1, name: "Arto Hellas", number: "040-123456" },
+    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
+    { id: 3, name: "Dan Abramov", number: "12-43-234345" },
+    { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
+
+  // This state is used to store entire details of persons from phonebook
+  const [allPersons, setAllPersons] = useState(filteredPersons);
+
 
   const addPerson = (event) => {
     event.preventDefault();
-    const already_exists = persons.filter((p) => p.name === newName);
+    const already_exists = filteredPersons.filter((p) => p.name === newName);
 
     if (already_exists.length) {
       // Its supposed to be backticks and not a quote for string formatting
       window.alert(`'${newName}' is already added to phonebook`);
     } else {
       const newPerson = {
-        id: persons.length + 1,
+        // Since state variable filteredPersons is used to store the filtered results
+        // It should not be used when adding a new phonebook contact
+        // allPersons should be used
+        id: allPersons.length + 1,
         name: newName,
         number: newNumber,
       };
-      setPersons(persons.concat(newPerson));
+
+      // When adding a new phoneboo contact, update both filteredPersons and
+      // allPersons so that the results get updated
+      setFilteredPersons(allPersons.concat(newPerson));
+      setAllPersons(allPersons.concat(newPerson));
     }
     setNewName("");
     setNewNumber("");
@@ -71,9 +98,30 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearchInput = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    // To make the search case insensitive, each string is 
+    // converted to lowercase. string1.includes(string2) returns True 
+    // when string2 is a substring of string1
+    const filterResult = allPersons.filter((p) =>
+      p.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredPersons(filterResult);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter
+        search={search}
+        handleSearchInput={handleSearchInput}
+        handleSearch={handleSearch}
+      />
+
       <AddPersonForm
         newName={newName}
         handleNameInput={handleNameInput}
@@ -81,7 +129,7 @@ const App = () => {
         handleNumberInput={handleNumberInput}
         addPerson={addPerson}
       />
-      <AllPersons persons={persons} />
+      <PersonDetails persons={filteredPersons} />
     </div>
   );
 };
